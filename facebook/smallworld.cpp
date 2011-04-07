93369e9ad888ddca6b94d598d27427d07b33d623
Rongzhou Shen <anticlockwise5@gmail.com>
2011-04-07 09:00:08 +0100

    It's a small world added, User Bin Crash updated and finished.

---
+++ facebook/smallworld.cpp
@@ -0,0 +1,154 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  smallworld.cpp
+ *
+ *    Description:  Facebook Puzzle - Snack Level: It's A Small World
+ *
+ *        Version:  1.0
+ *        Created:  04/06/2011 20:49:56
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Rongzhou Shen (rshen), anticlockwise5@gmail.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cfloat>
+
+using namespace std;
+
+struct point {
+    int id;
+    double x;
+    double y;
+
+    point (int n=-1, double i=0.0, double j=0.0) {
+        id = n;
+        x = i;
+        y = j;
+    }
+
+    double operator - (const point p) {
+        double a = x - p.x;
+        double b = y - p.y;
+        return a * a + b * b;
+    }
+};
+
+bool compare_x (const point &p1, const point &p2) {
+    return p1.x < p2.x;
+}
+
+bool compare_y (const point &p1, const point &p2) {
+    return p1.x < p2.x;
+}
+
+bool compare_d (const pair<point, double> &d1, const pair<point, double> &d2) {
+    return d1.second < d2.second;
+}
+
+void build_tree (vector<point> &tree, vector<point> friends, int depth, int index) {
+    int len = friends.size();
+    if (len == 0)
+        return;
+
+    point mid;
+    vector<point> left, right;
+    int i, m;
+    if (depth % 2 == 0) {
+        sort(friends.begin(), friends.end(), compare_x);
+    } else {
+        sort(friends.begin(), friends.end(), compare_y);
+    }
+
+    m = len / 2;
+    mid = friends[m];
+    tree[index] = mid;
+
+    for (i = 0; i < m; i++) {
+        left.push_back(friends[i]);
+    }
+    build_tree (tree, left, depth + 1, index * 2 + 1);
+
+    for (i = m + 1; i < len; i++) {
+        right.push_back(friends[i]);
+    }
+    build_tree (tree, right, depth + 1, index * 2 + 2);
+}
+
+void nearest_k (vector<point> tree, int cur_node, point p, int k,
+        vector< pair<point, double> > &min, double &k_dist, int depth) {
+    int axis = depth % 2;
+    double d;
+    int near, far;
+
+    if (cur_node < tree.size() && tree[cur_node].id != -1) {
+        if (axis == 0) {
+            d = p.x - tree[cur_node].x;
+        } else {
+            d = p.y - tree[cur_node].y;
+        }
+
+        near = (d <= 0 ? cur_node*2+1 : cur_node*2+2);
+        far  = (d <= 0 ? cur_node*2+2 : cur_node*2+1);
+
+        nearest_k (tree, near, p, k, min, k_dist, depth + 1);
+
+        if (d * d < k_dist)
+            nearest_k (tree, far, p, k, min, k_dist, depth + 1);
+
+        d = p - tree[cur_node];
+        if (d < k_dist) {
+            pair<point, double> pi(tree[cur_node], d);
+            min.push_back(pi);
+            push_heap(min.begin(), min.end(), compare_d);
+
+            int s = min.size();
+            if (s > k) {
+                int nremove = s - k;
+                int i;
+                for (i = 0; i < nremove; i++) {
+                    pop_heap(min.begin(), min.end(), compare_d);
+                    min.pop_back();
+                }
+
+                k_dist = min.front().second;
+            }
+        }
+    }
+}
+
+int main() {
+    vector<point> friends;
+    vector<point> tree;
+    point p;
+    int i, j, nfriends=0, nnodes=1;
+
+    while (cin >> p.id >> p.x >> p.y) {
+        friends.push_back(p);
+        nfriends++;
+    }
+
+    while (nnodes <= nfriends)
+        nnodes *= 2;
+    nnodes -= 1;
+
+    tree.resize(nnodes);
+    build_tree(tree, friends, 0, 0);
+
+    vector< pair<point, double> > min;
+    for (i = 0; i < nfriends; i++) {
+        double k_dist = DBL_MAX;
+        nearest_k(tree, 0, friends[i], 4, min, k_dist, 0);
+        sort(min.begin(), min.end(), compare_d);
+        cout << min[0].first.id << " " << min[1].first.id << ","
+             << min[2].first.id << "," << min[3].first.id << endl;
+        min.clear();
+    }
+}
--- facebook/usrbincrash.cpp
+++ facebook/usrbincrash.cpp
@@ -1,7 +1,95 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <climits>
+
+using namespace std;
+
+#define MAXN 100000000
+
+struct cargo {
+    string name;
+    int weight;
+    int value;
+
+    cargo (string n="", int w=0, int v=0) {
+        name = n;
+        weight = w;
+        value = v;
+    }
+};
+
+int gcd(int x, int y) {
+    int r;
+    if (x > y) {
+        int t = x;
+        x = y;
+        y = t;
+    }
+
+    r = y % x;
+    while (r != 0) {
+        y = x;
+        x = r;
+        r = y % x;
+    }
+    return x;
+}
 
 int main() {
+    int target;
+    vector<cargo> cargos;
+    vector<int> cost;
+    cargo ca;
+    cargo *p;
+    string line;
+    char name[1024];
+    int weight, value, g, i, j;
+    cost.resize(MAXN);
 
+    for (i = 0; i < MAXN; i++)
+        cost[i] = INT_MAX;
+
+    getline(cin, line);
+    sscanf(line.c_str(), "%d", &target);
+
+    while (getline(cin, line)) {
+        sscanf(line.c_str(), "%s %d %d", name, &weight, &value);
+        ca.name = name;
+        ca.weight = weight;
+        ca.value = value;
+        cargos.push_back(ca);
+    }
+
+    g = gcd(target, cargos[0].weight);
+    for (i = 1; i < (int)cargos.size(); i++) {
+        g = gcd(g, cargos[i].weight);
+    }
+
+    target = target / g;
+    for (i = 0; i < (int)cargos.size(); i++) {
+        p = &cargos[i];
+        p->weight /= g;
+    }
+
+    int size = cargos.size();
+    cost[0] = 0;
+    for (i = 0; i <= target; i++) {
+        for (j = 0; j < size; j++) {
+            p = &cargos[j];
+            if (p->weight > i) {
+                if (p->value < cost[i]) {
+                    cost[i] = p->value;
+                }
+            } else {
+                int w = i - p->weight;
+                int sum_value = cost[w] + p->value;
+                if (sum_value < cost[i]) {
+                    cost[i] = sum_value;
+                }
+            }
+        }
+    }
+
+    cout << cost[target] << endl;
 }
