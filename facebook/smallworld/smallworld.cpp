03edeb55cde86934254885adf907e22da651c12e
rshen <anticlockwise5@gmail.com>
2011-04-14 15:31:55 +0100

    Modified It's a small world algorithm

--- facebook/smallworld/smallworld.cpp
+++ facebook/smallworld/smallworld.cpp
@@ -20,139 +20,168 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 #include <algorithm>
 #include <cfloat>
 
 using namespace std;
 
 struct point {
-    int id;
-    double x;
-    double y;
+    int number;
+    double xcoord;
+    double ycoord;
 
-    point (int n=-1, double i=0.0, double j=0.0) {
-        id = n;
-        x = i;
-        y = j;
+    point(int n=0, double x=0, double y=0) {
+        number = n;
+        xcoord = x;
+        ycoord = y;
     }
 
-    double operator - (const point p) {
-        double a = x - p.x;
-        double b = y - p.y;
-        return a * a + b * b;
+    double operator [](int axis) {
+        if (axis == 0)
+            return xcoord;
+        else
+            return ycoord;
+    }
+};
+
+typedef pair<double, point> nloc;
+
+struct person {
+    point location;
+    person *left;
+    person *right;
+
+    person(point loc,
+            person *l, person *r) {
+        location = loc;
+        left = l;
+        right = r;
     }
 };
 
 bool compare_x (const point &p1, const point &p2) {
-    return p1.x < p2.x;
+    return p1.xcoord < p2.xcoord;
 }
 
 bool compare_y (const point &p1, const point &p2) {
-    return p1.y < p2.y;
+    return p1.ycoord < p2.ycoord;
 }
 
-bool compare_d (const pair<point, double> &d1, const pair<point, double> &d2) {
-    return d1.second < d2.second;
-}
+person *build_tree(vector<point> &coords,
+        int depth=0) {
+    if (coords.empty()) {
+        return NULL;
+    }
 
-void build_tree (vector<point> &tree, vector<point> friends, int depth, int index) {
-    int len = friends.size();
-    if (len == 0)
-        return;
+    int k = 2;
+    int axis = depth % k + 1;
+    int median = coords.size() / 2;
+    person *p = NULL;
 
-    point mid;
-    vector<point> left, right;
-    int i, m;
-    if (depth % 2 == 0) {
-        sort(friends.begin(), friends.end(), compare_x);
+    if (axis == 0) {
+        sort(coords.begin(), coords.end(), compare_x);
     } else {
-        sort(friends.begin(), friends.end(), compare_y);
+        sort(coords.begin(), coords.end(), compare_y);
     }
 
-    m = len / 2;
-    mid = friends[m];
-    tree[index] = mid;
-
-    for (i = 0; i < m; i++) {
-        left.push_back(friends[i]);
+    int i;
+    vector<point> lower_half, higher_half;
+    for (i = 0; i < median; i++) {
+        lower_half.push_back(coords[i]);
     }
-    build_tree (tree, left, depth + 1, index * 2 + 1);
-
-    for (i = m + 1; i < len; i++) {
-        right.push_back(friends[i]);
+    for (i = median + 1; i < coords.size(); i++) {
+        higher_half.push_back(coords[i]);
     }
-    build_tree (tree, right, depth + 1, index * 2 + 2);
+    p = new person(coords[median],
+            build_tree(lower_half, depth+1),
+            build_tree(higher_half, depth=1));
+    return p;
 }
 
-void nearest_k (vector<point> tree, int cur_node, point p, int k,
-        vector< pair<point, double> > &min, double &k_dist, int depth) {
-    int axis = depth % 2;
-    double d;
-    int near, far;
+bool compare_d(const nloc &l1, const nloc &l2) {
+    return l1.first < l2.first;
+}
 
-    if (cur_node < tree.size() && tree[cur_node].id != -1) {
-        if (axis == 0) {
-            d = p.x - tree[cur_node].x;
-        } else {
-            d = p.y - tree[cur_node].y;
+bool compare_n(const point &p1, const point &p2) {
+    return p1.number < p2.number;
+}
+
+double distance(point *c1, point *c2) {
+    return pow(c1->xcoord - c2->xcoord, 2) + pow(c1->ycoord - c2->ycoord, 2);
+}
+
+void add_nearest(vector<nloc> &nearest, person *root, point coord) {
+    double d = distance(&(root->location), &coord);
+    if (nearest.size() < 4 || d < nearest.back().first) {
+        if (nearest.size() >= 4) {
+            nearest.pop_back();
         }
+        nearest.push_back(make_pair(d, root->location));
+        sort(nearest.begin(), nearest.end(), compare_d);
+    }
+}
 
-        near = (d <= 0 ? cur_node*2+1 : cur_node*2+2);
-        far  = (d <= 0 ? cur_node*2+2 : cur_node*2+1);
+void nearest(vector<nloc> &nbs, person *root, point coord, int depth=0) {
+    int axis = depth % 2 + 1;
 
-        nearest_k (tree, near, p, k, min, k_dist, depth + 1);
+    if (root->left == NULL && root->right == NULL) {
+        add_nearest(nbs, root, coord);
+        return;
+    }
 
-        if (d * d < k_dist)
-            nearest_k (tree, far, p, k, min, k_dist, depth + 1);
+    person *nearer=NULL, *further=NULL;
+    if (root->right == NULL || (root->left != NULL && coord[axis] <= root->location[axis])) {
+        nearer = root->left;
+        further = root->right;
+    } else {
+        nearer = root->right;
+        further = root->left;
+    }
 
-        d = p - tree[cur_node];
-        if (d < k_dist) {
-            pair<point, double> pi(tree[cur_node], d);
-            min.push_back(pi);
-            push_heap(min.begin(), min.end(), compare_d);
+    nearest(nbs, nearer, coord, depth+1);
 
-            int s = min.size();
-            if (s > k) {
-                int nremove = s - k;
-                int i;
-                for (i = 0; i < nremove; i++) {
-                    pop_heap(min.begin(), min.end(), compare_d);
-                    min.pop_back();
-                }
-
-                k_dist = min.front().second;
-            }
+    if (further != NULL) {
+        if (nbs.size() < 4 || pow((coord[axis] - root->location[axis]), 2) < nbs.back().first) {
+            nearest(nbs, further, coord, depth+1);
         }
     }
+
+    add_nearest(nbs, root, coord);
 }
 
 int main(int argc, char *argv[]) {
-    vector<point> friends;
-    vector<point> tree;
-    point p;
-    int i, j, nfriends=0, nnodes=1;
-
     ifstream input(argv[1]);
+    int number;
+    double x, y;
+    vector<point> coords;
+    point p;
+    int i, j;
 
-    while (input >> p.id >> p.x >> p.y) {
-        friends.push_back(p);
-        nfriends++;
+    while (input >> number >> x >> y) {
+        p.number = number;
+        p.xcoord = x;
+        p.ycoord = y;
+        coords.push_back(p);
     }
 
-    while (nnodes <= nfriends)
-        nnodes *= 2;
-    nnodes -= 1;
+    person *tree = build_tree(coords);
+    sort(coords.begin(), coords.end(), compare_n);
+    int size = coords.size();
+    vector<nloc> nearest_3;
 
-    tree.resize(nnodes);
-    build_tree(tree, friends, 0, 0);
-
-    vector< pair<point, double> > min;
-    for (i = 0; i < nfriends; i++) {
-        double k_dist = DBL_MAX;
-        nearest_k(tree, 0, friends[i], 4, min, k_dist, 0);
-        sort(min.begin(), min.end(), compare_d);
-        cout << min[0].first.id << " " << min[1].first.id << ","
-             << min[2].first.id << "," << min[3].first.id << endl;
-        min.clear();
+    for (i = 0; i < size; i++) {
+        nearest(nearest_3, tree, coords[i]);
+        cout << coords[i].number << " ";
+        for (j = 0; j < 3; j++) {
+            cout << nearest_3[j+1].second.number;
+            if (j != 2) {
+                cout << ",";
+            }
+        }
+        cout << endl;
+        nearest_3.clear();
     }
+
+    return 0;
 }
